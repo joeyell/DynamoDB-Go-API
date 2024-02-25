fb70eb60a510e5e0f4b3b0c43f9ae6dff380b7fe
joeyelland@gmail.com <joeyelland@gmail.com>
2024-02-25 21:31:16 +0000

    Added DynamoDB connection and simple get ability

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module databaseAPI
 go 1.22.0
 
 require (
-	github.com/aws/aws-lambda-go v1.46.0 // indirect
-	github.com/valyala/fastjson v1.6.4 // indirect
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/aws/aws-sdk-go v1.50.25
+	github.com/valyala/fastjson v1.6.4
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/valyala/fastjson"
 )
+
+type UserData struct {
+	User_id  string `json:"user_id"`
+	Eligible bool   `json:"eligible"`
+}
+
+const tableName string = "user_data"
 
 func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	ApiResponse := events.APIGatewayProxyResponse{}
@@ -14,11 +26,28 @@ func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProx
 	switch request.HTTPMethod {
 	case "GET":
 		// Obtain the QueryStringParameter
-		name := request.QueryStringParameters["name"]
+		name := string(request.QueryStringParameters["name"])
 		if name != "" {
-			ApiResponse = events.APIGatewayProxyResponse{Body: "Hey " + name + " welcome! ", StatusCode: 200}
+			user := UserData{User_id: name}
+			err := user.dynamoGET()
+			if err != nil {
+
+				ApiResponse = events.APIGatewayProxyResponse{
+					StatusCode: 500,
+					Body:       fmt.Sprintf("Error: %s", err.Error()),
+				}
+			} else {
+				jsonString, _ := json.Marshal(user)
+				ApiResponse = events.APIGatewayProxyResponse{
+					StatusCode: 200,
+					Body:       string(jsonString),
+				}
+			}
 		} else {
-			ApiResponse = events.APIGatewayProxyResponse{Body: "Error: Query Parameter name missing", StatusCode: 500}
+			ApiResponse = events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Body:       "Error: Query Parameter name missing",
+			}
 		}
 
 	case "POST":
@@ -37,6 +66,37 @@ func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProx
 	return ApiResponse, nil
 }
 
+func (user *UserData) dynamoGET() error {
+	sess := session.Must(session.NewSession())
+
+	// Defer the closure of the session
+	//defer sess.Close()
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"user_id": {
+				S: aws.String(user.User_id),
+			},
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to call GetItem: %s", err)
+	}
+
+	if len(result.Item) == 0 {
+		return fmt.Errorf("item not found for user_id: %s", user.User_id)
+	}
+
+	if err := dynamodbattribute.UnmarshalMap(result.Item, &user); err != nil {
+		return fmt.Errorf("failed to unmarshal Record: %s", err)
+	}
+	return nil
+}
 func main() {
 	// Starts the handler for AWS Lambda
 	lambda.Start(HandleRequest)
